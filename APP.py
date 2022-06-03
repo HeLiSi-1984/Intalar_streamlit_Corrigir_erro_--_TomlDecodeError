2a228dffa2ee295c55db6f5d53478ff82dabb39c
helisipgc <helisi.pgc@gmail.com>
2022-06-03 13:51:02 -0300

    023/06/2022

---
+++ APP.py
@@ -0,0 +1,18 @@
+# Nome do app = streamlit run APP.py
+
+# pip install streamlit
+# pip install --upgrade pip
+
+
+import streamlit as st
+
+
+def main():
+    st.write("MEU PRIMEIRO PROJETO STREAMLIT!!!")
+
+
+if __name__ == '__main__':
+    st.set_page_config(page_title='MEU APP',layout='wide')
+    main()
+
+#Tchau brigadu!!
